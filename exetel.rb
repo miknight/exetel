2676da3948623985d0280f7a55e0f533d024a938
Michael Knight <jedimike@gmail.com>
2010-10-31 23:38:11 +1100

    Added more API calls and the ability to load arbitrary data in.

--- exetel.rb
+++ exetel.rb
@@ -1,23 +1,35 @@
 require 'rexml/document'
 require 'net/https'
 require 'uri'
+require 'date'
 
 class Exetel
 
   DATA_URL = "https://www.exetel.com.au/members/usagemeter_xml.php?{USERNAME},{PASSWORD}"
 
   # Accept either a username and password or a file name.
-  def initialize(user_or_file, password = nil)
+  # If neither are passed, data needs to be sent in via data=().
+  def initialize(user_or_file = nil, password = nil)
+    return if user_or_file.nil?
     if password.nil?
       if !File.exists?(user_or_file)
         puts "File #{user_or_file} does not exist. Exiting..."
         exit 1
       end
-      data = File.read(user_or_file)
+      @data = File.read(user_or_file)
     else
       url = DATA_URL.sub('{USERNAME}', user_or_file).sub('{PASSWORD}', password)
-      data = fetch(url)
+      @data = fetch(url)
     end
+    parse(@data)
+  end
+
+  def data
+    @data
+  end
+
+  def data=(data)
+    @data = data
     parse(data)
   end
 
@@ -27,6 +39,33 @@ class Exetel
 
   def offpeak_usage
     @doc.elements['CurrentMonthUsage'].elements['OffpeakDownload'].text.to_f
+  end
+
+  def peak_limit
+    @doc.elements['PlanDetails'].elements['PeakTimeDownloadInMB'].text.to_f
+  end
+
+  def offpeak_limit
+    @doc.elements['PlanDetails'].elements['OffpeakTimeDownloadInMB'].text.to_f
+  end
+
+  def peak_percentage
+    peak_usage / peak_limit * 100
+  end
+
+  def offpeak_percentage
+    return 0 if offpeak_limit == 0
+    offpeak_usage / offpeak_limit * 100
+  end
+
+  def reported_date
+    @doc.elements['CurrentMonthUsage'].elements['UpdateDatetime'].text
+  end
+
+  def month_percentage
+    d = Date.parse(reported_date)
+    days_in_month = Date.new(d.year, d.month, -1).day
+    (d.day - 1.0) / days_in_month * 100
   end
 
   private
--- tests/exetel_test.rb
+++ tests/exetel_test.rb
@@ -4,16 +4,33 @@ require File.dirname(__FILE__) + '/../exetel'
 
 class ExetelTest < Test::Unit::TestCase
 
+  PEAK_USAGE = 31768.18
+  OFFPEAK_USAGE = 23307.4
+  PEAK_LIMIT = 36000
+
   def setup
     @exetel = Exetel.new(File.dirname(__FILE__) + '/exetel.xml')
   end
 
   def test_peak_usage
-    assert_equal 31768.18, @exetel.peak_usage
+    assert_equal PEAK_USAGE, @exetel.peak_usage
+  end
+
+  def test_peak_percentage
+    expected_percentage = PEAK_USAGE / PEAK_LIMIT * 100
+    assert_equal expected_percentage, @exetel.peak_percentage
   end
 
   def test_offpeak_usage
-    assert_equal 23307.4, @exetel.offpeak_usage
+    assert_equal OFFPEAK_USAGE, @exetel.offpeak_usage
+  end
+
+  def test_offpeak_percentage
+    assert_equal 0, @exetel.offpeak_percentage
+  end
+  
+  def test_month_percentage
+    assert_equal ((27 - 1.0) / 31 * 100), @exetel.month_percentage
   end
 
 end
